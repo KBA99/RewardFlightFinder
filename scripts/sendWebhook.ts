32ba77d03a4867043573cfb78a05e723601cd85b
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-12-21 14:16:09 +0000

    feat: add parameter for specifying flight type

--- scripts/sendWebhook.ts
+++ scripts/sendWebhook.ts
@@ -1,7 +1,7 @@
 import { config as env} from '../config'
 import Axios from 'axios-https-proxy-fix';
 
-export const sendWebhook = async (dates: any[]) => {
+export const sendWebhook = async (dates: any[], flightType: string) => {
 	const data = { 
 		embeds: [
 			{
@@ -12,7 +12,7 @@ export const sendWebhook = async (dates: any[]) => {
 					icon_url:
 						'https://cdn.discordapp.com/attachments/1028795460892229722/1028797236483739688/british-airways-eps-vector-logo.png',
 				},
-				title: 'Reward Flight Found!',
+				title: `${flightType} Reward Flight Found!`,
 				url: `https://rewardflightfinder.com/calendar?numberOfPassengers=1&tclass=Economy&tValue=economy&jType=return&dPlace=London%20(LGW%2C%20LHR%2C%20LCY%2C%20LTN%2C%20SEN%2C%20STN)&dId=LON&aPlace=Accra%20(ACC)&aId=ACC&economy=true&premium=true&first=true&business=true`,
 				thumbnail: {
 					url: 'https://cdn.discordapp.com/attachments/1028795460892229722/1028797236483739688/british-airways-eps-vector-logo.png',
