622c40ff3944fa1b9c478be4da0bcd1e6bddd2cc
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2023-03-25 14:18:04 +0000

    Update config file

--- config.ts
+++ config.ts
@@ -2,5 +2,7 @@ export const config = {
 	check_out_flights: false,
 	check_return_flights: true,
 	cooldown_time: 20_000, //ms, 20_0000 = 20 seconds
-	webhookUrl: ''
+	webhookUrl: '',
+	baseLocation: '',
+	desitnation: '',
 }
